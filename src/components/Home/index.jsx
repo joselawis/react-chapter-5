1343188722e7a3e08b6fa2da674247474af1ff4c
joselawis <joselawis@gmail.com>
2022-04-13 17:39:04 +0200

    Redux store

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { bool } from 'prop-types';
+import { connect } from 'react-redux';
 
-function Home() {
+function Home(props) {
+  const { isMobile } = props;
   return (
     <div className="Home">
       <h1>Home</h1>
+      <p>
+        You are using:
+        <strong> {isMobile ? 'mobile' : 'desktop'}</strong>
+      </p>
     </div>
   );
 }
 
-export default Home;
+// Toma una parte del estado y lo pasa al componente como prop.
+// El estado que recibe es el estado de redux y dentro del mismo tendrá todos los reductores que
+// ha definido en rootReducer y luego devuelve un objecto con los datos que necesita para enviarlos al componente
+function mapStateToProps(state) {
+  return { isMobile: state.device.isMobile };
+}
+
+function mapDispatchToProps() {
+  return {};
+}
+
+Home.propTypes = { isMobile: bool };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,31 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 
+// Rutas
 import AppRoutes from './routes';
 
-render(
-  <Router>
-    <AppRoutes />
-  </Router>,
-  document.getElementById('root')
-);
+// Store de redux
+import configureStore from './shared/redux/configureStore';
+
+// Configure store de Redux
+const store = configureStore(window.initialState);
+
+// DOM
+const rootElement = document.getElementById('root');
+
+// Envoltorio de la aplicación
+const renderApp = (Component) => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <Component />
+      </Router>
+    </Provider>,
+    rootElement
+  );
+};
+
+renderApp(AppRoutes);
